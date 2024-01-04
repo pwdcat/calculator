e9c77180603ab7580a444592441fe27924f3850c
pwdcat <113306139+pwdcat@users.noreply.github.com>
2024-01-04 13:55:16 -0500

    Improved Visuals, Started Functionality Buttons have their click events for their respective type add an expression and result text box

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-let first;
-let second;
+let inputs = ['', ''];
 let operator;
 
 function add(a, b){
@@ -18,6 +17,39 @@ function divide(a, b){
     return a / b;
 }
 
-function operate(operator, first, second){
-    
+function operate(operator, inputs){
+
 }
+
+let textBox = document.getElementById("result");
+let numbers = document.querySelectorAll(".number");
+
+numbers.forEach((button) =>{
+    button.addEventListener('click', () => {
+        if(operator){
+            inputs[1] += button.textContent;
+            textBox.textContent = inputs[1];
+        }
+        else{
+            inputs[0] += button.textContent;
+            textBox.textContent = inputs[0];
+        }
+    });
+});
+
+let expressionBox = document.getElementById("expression")
+let operators = document.querySelectorAll(".operator");
+
+operators.forEach((button) =>{
+    button.addEventListener('click', () => {
+        operator = button.textContent;
+        expressionBox.textContent = inputs[0] + ' ' + operator;
+    });
+});
+
+let equals = document.getElementById("equals");
+
+equals.addEventListener('click', () => {
+    expressionBox.textContent = inputs[0] + ' ' + operator + ' ' + inputs[1] + ' = ';
+    operate(operator, inputs);
+});
